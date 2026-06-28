f1c88cec1e280147a3b077f239305e8e220e2570
Lewmitha Kithuldeniya <studioslewmithas@gmail.com>
2026-06-29 05:05:51 +0530

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,6 @@
+module.exports = {
+    giphyApiKey: process.env.GIPHY_API_KEY || 'dc6zaTOxFJmzC',
+    ownerNumber: process.env.OWNER_NUMBER || '94750915292',
+    botName: 'LEWZ MD',
+    ownerName: 'LEWZ MD'
+};
