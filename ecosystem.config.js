262ca6b8ecffbbf9ac2a469c495e728d4de3636a
Lewmitha Kithuldeniya <studioslewmithas@gmail.com>
2026-06-29 16:43:16 +0530

    Add files via upload

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   apps : [{
-    name: "old-studio-bot",
+    name: "lewz-md-bot",
     script: "./index.js",
     watch: false,
     autorestart: true,
--- settings.js
+++ settings.js
@@ -2,5 +2,5 @@ module.exports = {
     giphyApiKey: process.env.GIPHY_API_KEY || 'dc6zaTOxFJmzC',
     ownerNumber: process.env.OWNER_NUMBER || '94750915292',
     botName: 'LEWZ MD',
-    ownerName: 'LEWZ MD'
+    ownerName: 'Lewz Kithuldeniya'
 };
